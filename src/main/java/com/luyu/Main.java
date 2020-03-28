e65f0978e4651b0d7d63b3996fecc76b7627a82b
luyu <luyu_6666@qq.com>
2020-03-28 23:03:48 +0800

    second commit

--- src/main/java/com/luyu/Main.java
+++ src/main/java/com/luyu/Main.java
@@ -12,6 +12,7 @@ public class Main {
         System.out.println("-------------------------");
         //以目标对象换回一个代理对象
         Fruit appleProxy = (Fruit) DynamicProxy.GetProxy(appleInstance);
+        //同样是调用canbejuice方法,代理对象多执行了一些代码
         appleProxy.canbejuice();
     }
 }
