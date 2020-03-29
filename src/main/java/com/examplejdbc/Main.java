a133acc8dc610e3711fa8c36a4f9411a8b77b960
luyu <luyu_6666@qq.com>
2020-03-29 19:01:23 +0800

    commit

--- src/main/java/com/examplejdbc/Main.java
+++ src/main/java/com/examplejdbc/Main.java
@@ -1,22 +1,27 @@
 package com.examplejdbc;
 
-
-import com.mysql.cj.jdbc.Driver;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        //测试mysql连接
-        String url = "jdbc:mysql://localhost:3306/test" +
-                "?serverTimezone=Asia/Shanghai" +
-                "&useSSL=false" +
-                "&user=root" +
-                "&password=mysql";
+    public static void main(String[] args) throws Exception {
+        //读入数据库配置
+        Properties properties = new Properties();
+        InputStream in = new FileInputStream(new File("E:/下载/JavaInteresting/src/main/resources/jdbc.properties"));
+        properties.load(in);
+
+        String url = properties.getProperty("jdbcUrl");
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+        String driver = properties.getProperty("driver");
+
         //拿到连接对象
-        Connection connection = DriverManager.getConnection(url);
+        Connection connection = DriverManager.getConnection(url, user, password);
         System.out.println(connection);
         connection.close();
     }
