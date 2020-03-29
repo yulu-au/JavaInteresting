22abac7da913f97ce491bc621023f0815f8a1728
luyu <luyu_6666@qq.com>
2020-03-29 18:04:47 +0800

    jdbc example

---
+++ src/main/java/com/examplejdbc/Main.java
@@ -0,0 +1,25 @@
+package com.examplejdbc;
+
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        //测试mysql连接
+        //拿到驱动对象
+        Driver driver = new com.mysql.cj.jdbc.Driver();
+        String url = "jdbc:mysql://localhost:3306/test" +
+                "?serverTimezone=Asia/Shanghai" +
+                "&useSSL=false" +
+                "&user=root" +
+                "&password=mysql";
+        //拿到连接对象
+        Connection connection = DriverManager.getConnection(url);
+        System.out.println(connection);
+        connection.close();
+    }
+}
